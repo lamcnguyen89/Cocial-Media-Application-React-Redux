b6958700bca222ae8be2d4942e15508515841ee9
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-15 15:33:35 -0500

    I can post a new user. Before I was having isues with Postman because when selecting Body, I next selected form-data. Should have selected x-www-form-urlencoded

--- routes/api/authRoute.js
+++ routes/api/authRoute.js
@@ -2,10 +2,59 @@
 const express = require('express');
 const router = express.Router();
 
+// Module for processing an email:
+const gravatar = require('gravatar');
+
+//Module to encrypt the password so it doesn't just show up in the database in plain form:
+const bcrypt = require('bcryptjs');
+
+// Load user Model:
+const User = require('../../models/User');
+
+
 //@route GET api/auth/test
 //@desc Tests authentication route
 //@access Public
 router.get('/test', (req,res)=> res.json({msg: "Authentication Route Works"}))
 ;
+
+//@route GET api/auth/register
+//@desc registers user
+//@access Public
+router.post('/register', (req,res)=> {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user) {
+                return res.status(400).json({message: 'Email already exists!'})
+            } else {
+
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //size
+                    r: 'r', // Rating
+                    d: 'mm' //Default
+                });
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+
+                // BCrypt used to encrypt the password text as it is entered into the user database so that it isn't plainly visible to anyone who has access to the database.
+                bcrypt.genSalt(10, (err, salt) => {
+                    if (err) throw err;
+                    bcrypt.hash(newUser.password, salt, (err, hash)=> {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err))
+                    })
+                })
+            }
+        });
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,25 @@ require("dotenv").config();
 const express = require('express');
 const path = require("path");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const db = require('./config/keys').MONGO_URI;
 const auth = require('./routes/api/authRoute');
-const profile = require('./routes/api/profilesRoute');
+const profile = require('./routes/api/profileRoute');
 const posts = require('./routes/api/postsRoute');
 
 const app = express()
 
+// Body-Parser middleware. This middleware will allow the application to pick up text inputs from the web browser and translate them to serverside requests:
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
 // Connect to MongoDB database through Mongoose
 mongoose
-    .connect(db)
+    .connect(db,
+        { useNewUrlParser: true, 
+            useUnifiedTopology: true, 
+            useCreateIndex: true, 
+            useFindAndModify: true })
     .then(()=>console.log("mongoDB successfully connected!"))
     .catch(err => console.log(err));
 
