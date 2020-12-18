67a094b7591946f9cd84a3c33888fd24af07713b
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-18 05:17:37 -0500

    Created add profile route

--- routes/api/profileRoute.js
+++ routes/api/profileRoute.js
@@ -1,35 +1,97 @@
 // Routes for showing social media user information like bio, birth date, etc... to other users of this social media application
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
+const mongoose = require("mongoose");
+const passport = require("passport");
 
 // Load Profile Model:
-const Profile = require('../../models/Profile');
+const Profile = require("../../models/Profile");
 // Load User Model:
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 //@route GET api/profiles/test
 //@desc Tests profiles route
 //@access Public
-router.get('/test', (req,res)=> res.json({msg: "Profiles Route Works"}));
+router.get("/test", (req, res) => res.json({ msg: "Profiles Route Works" }));
 
 //@route GET api/profiles
 //@desc Get current logged in user's profile
 //@access Private
-router.get('/', passport.authenticate('jwt', {session: false}), (req, res)=>{
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
-        .then(profile => {
-            if(!profile) {
-                errors.noprofile = 'There is no profile for this user'
-                return res.status(404).json(errors)
+      .then((profile) => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+//@route POST api/profiles
+//@desc create user's profile
+//@access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.status) profileFields.status = req.body.status;
+    // Skills - Split into an array:
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    // Social -
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      if (profile) {
+        // Update the profile
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        )
+        .then(profile => res.json(profile));
+      } else {
+        // Create:
+
+        // Check for duplicate handles:
+        Profile.findOne({ handle: profileFields.handle }).then(profile =>{
+            if(profile) {
+                errors.handle = 'This handle already exists';
+                res.status(400).json(errors);
             }
-            res.json(profile);
+
+            // Save Profile:
+            new Profile(profileFields).save().then(profile => res.json(profile));
         })
-        .catch(err=>res.status(404).json(err))
-});
+      }
+    });
+  }
+);
 
 module.exports = router;
