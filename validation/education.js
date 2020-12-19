432b447058edc79389e81272c02415f31f823030
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-19 18:24:54 -0500

    Added routes for experience and education.

---
+++ validation/education.js
@@ -0,0 +1,30 @@
+// File where we create the rules for adding education information to the profile:
+
+const isEmpty = require("./is-empty");
+
+const Validator = require("validator");
+
+module.exports = function validateEducationInput(data) {
+  let errors = {};
+
+  data.school = !isEmpty(data.school) ? data.school : "";
+  data.degree = !isEmpty(data.degree) ? data.degree : "";
+  data.fieldofstudy = !isEmpty(data.fieldofstudy) ? data.fieldofstudy : "";
+
+  if (Validator.isEmpty(data.school)) {
+    errors.school = "School field is required";
+  }
+
+  if (Validator.isEmpty(data.degree)) {
+    errors.degree = "Degree field is required";
+  }
+
+  if (Validator.isEmpty(data.fieldofstudy)) {
+    errors.fieldofstudy = "Fieldofstudy field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
