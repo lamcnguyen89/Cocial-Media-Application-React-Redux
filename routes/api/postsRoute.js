393da9ad9540193f48416897cbe4b9714c000ef7
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-20 16:33:05 -0500

    Added comment addition and deletion routes

--- routes/api/postsRoute.js
+++ routes/api/postsRoute.js
@@ -139,11 +139,9 @@ router.post(
             post.likes.filter((like) => like.user.toString() === req.user.id)
               .length === 0
           ) {
-            return res
-              .status(400)
-              .json({
-                unliked: "User already disliked or has not rated this post",
-              });
+            return res.status(400).json({
+              unliked: "User already disliked or has not rated this post",
+            });
           }
 
           // Get the remove index:
@@ -161,6 +159,78 @@ router.post(
           res.status(404).json({ postnotfound: "Post was not found" })
         );
     });
+  }
+);
+
+//@route POST api/posts/comment/:id
+//@desc Add a comment to a post
+//@access Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation:
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then((post) => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id,
+        };
+
+        // Add to comments array:
+        post.comments.unshift(newComment);
+
+        //Save Added comment to database:
+        post.save().then((post) => res.json(post));
+      })
+      .catch((err) =>
+        res.status(404).json({ postnotfound: "Post was not found" })
+      );
+  }
+);
+
+//@route DELETE api/posts/comment/:id/:comment_id
+//@desc Remove a comment from a post
+//@access Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then((post) => {
+        // Check to see if the comment exists:
+        if (
+          post.comments.filter(
+            (comment) => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ nonexistentcomment: "Comment does not exist" });
+        }
+
+        // Get the remove index:
+        const removeIndex = post.comments
+          .map((item) => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice comment out of array:
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then((post) => res.json(post));
+      })
+      .catch((err) =>
+        res.status(404).json({ postnotfound: "Post was not found" })
+      );
   }
 );
 
