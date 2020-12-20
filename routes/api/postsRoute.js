af491ebcfdcd0cb54ae1484d24edbd7cb808b8b3
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-20 14:44:02 -0500

    Added the like and dislike route for a post

--- routes/api/postsRoute.js
+++ routes/api/postsRoute.js
@@ -7,6 +7,8 @@ const passport = require("passport");
 
 // Post Model:
 const Post = require("../../models/Post");
+// Profile Model:
+const Profile = require("../../models/Profile");
 
 // Validation:
 const validatePostInput = require("../../validation/post");
@@ -23,7 +25,8 @@ router.get("/", (req, res) => {
   Post.find()
     .sort({ date: -1 })
     .then((posts) => res.json(posts))
-    .catch((err) => res.status(404)).json({nopostsfound: 'No posts found'})
+    .catch((err) => res.status(404))
+    .json({ nopostsfound: "No posts found" });
 });
 
 //@route GET api/posts/:id
@@ -32,7 +35,9 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   Post.findById(req.params.id)
     .then((post) => res.json(post))
-    .catch((err) => res.status(404).json({nopostfound: 'No post found with that ID'}));
+    .catch((err) =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
 });
 
 //@route POST api/posts
@@ -58,6 +63,104 @@ router.post(
     });
 
     newPost.save().then((post) => res.json(post));
+  }
+);
+
+//@route DELETE api/posts/:id
+//@desc Delete a post
+//@access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          // Check for the post owner:
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized " });
+          }
+
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: "Post was not found" })
+        );
+    });
+  }
+);
+
+//@route POST api/posts/like/:id
+//@desc Like a post
+//@access Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          // Check to see if user already liked the post:
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          }
+
+          // Add user id to likes array:
+          post.likes.unshift({ user: req.user.id });
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: "Post was not found" })
+        );
+    });
+  }
+);
+
+//@route POST api/posts/unlike/:id
+//@desc unlike a post
+//@access Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          // Check to see if user already liked the post:
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({
+                unliked: "User already disliked or has not rated this post",
+              });
+          }
+
+          // Get the remove index:
+          const removeIndex = post.likes
+            .map((item) => item.user.toString())
+            .indexOf(req.user.id);
+
+          // Splice found user out of the array:
+          post.likes.splice(removeIndex, 1);
+
+          // Save the unlike:
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: "Post was not found" })
+        );
+    });
   }
 );
 
