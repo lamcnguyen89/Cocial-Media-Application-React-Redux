d6c1596f01388d1132cb61083fd6d3f20e86979f
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-23 17:11:07 -0500

    Created Register and Login react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/authentication/Register";
+import Login from "./components/authentication/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <Route exact path="/" component={Landing} /> 
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </header>
+      </div>
+    </Router>
   );
 }
 
