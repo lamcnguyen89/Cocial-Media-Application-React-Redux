9ffe1b5c2f96c5c1ea3680e1ad05f4b9b6b4e579
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-29 05:08:56 -0500

    Added the edit profile function

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,54 +1,75 @@
-import axios from 'axios';
+import axios from "axios";
 
 import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
-} from './types';
+  GET_ERRORS,
+  SET_CURRENT_USER,
+} from "./types";
 
 // Get current profile
-export const getCurrentProfile = () => dispatch => {
+export const getCurrentProfile = () => (dispatch) => {
   dispatch(setProfileLoading());
   axios
-    .get('/api/profile')
-    .then(res =>
+    .get("/api/profile")
+    .then((res) =>
       dispatch({
         type: GET_PROFILE,
-        payload: res.data
+        payload: res.data,
       })
     )
-    .catch(err =>
+    .catch((err) =>
       dispatch({
         type: GET_PROFILE,
-        payload: {}
+        payload: {},
       })
     );
 };
 
 // Create Profile
-export const createProfile = (profileData, history) => dispatch => {
+export const createProfile = (profileData, history) => (dispatch) => {
   axios
-    .post('/api/profile', profileData)
-    .then(res => history.push('/dashboard'))
-    .catch(err =>
+    .post("/api/profile", profileData)
+    .then((res) => history.push("/dashboard"))
+    .catch((err) =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
+        payload: err.response.data,
       })
     );
+};
+
+// Delete account and profile
+export const deleteAccount = () => (dispatch) => {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then((res) =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {},
+        })
+      )
+      .catch((err) =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data,
+        })
+      );
+  }
 };
 
 // Profile loading
 export const setProfileLoading = () => {
   return {
-    type: PROFILE_LOADING
+    type: PROFILE_LOADING,
   };
 };
 
 // Clear profile
 export const clearCurrentProfile = () => {
   return {
-    type: CLEAR_CURRENT_PROFILE
+    type: CLEAR_CURRENT_PROFILE,
   };
 };
