58fc917c642a9467d2d476d7811b39d6ab53e376
Lam Nguyen <lamcnguyen89@gmail.com>
2020-12-30 01:43:05 -0500

    Added components and set up redux to display a public profile for an individual user

---
+++ client/src/components/profile/ProfileCreds.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProfileCreds extends Component {
+    render() {
+        return (
+            <div>
+              <h1>TODO: PROFILE QUALIFICATIONS</h1>  
+            </div>
+        )
+    }
+}
+
+export default ProfileCreds;
